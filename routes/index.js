4fd9b212da45c8ba381d668f292deac1303121c2
tonymtz <atony.mtz@gmail.com>
2020-09-28 23:43:45 -0700

    V0.0.1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const meta = {
+  title: 'El Culto al Perro Café',
+  description: 'Cafetería Indie Experimental. Cafetería que entiende a la gente que trabaja. Hermosillo, Sonora.',
+  url: 'https://cultoperrocafe.com',
+  image: 'https://cultoperrocafe.com/images/social.jpg'
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'El Culto al Perro Café' });
+  res.render('index', meta);
 });
 
 module.exports = router;
