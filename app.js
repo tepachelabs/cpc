81e7197e02408917ef71baa2278ec4d8371b61f2
tonymtz <atony.mtz@gmail.com>
2020-11-09 16:32:59 -0700

    Adding subscribe form

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var stylus = require('stylus');
 
 var captiveRouter = require('./routes/captive');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var menuRouter = require('./routes/menu');
 
 var app = express();
@@ -25,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/captive', captiveRouter);
-app.use('/users', usersRouter);
 app.use('/menu', menuRouter);
 
 // catch 404 and forward to error handler
