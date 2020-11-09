eeccb740d612e0f3a6701c33fe6ce5baa98b5f5d
tonymtz <atony.mtz@gmail.com>
2020-11-08 21:59:06 -0700

    Adding menu

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var stylus = require('stylus');
 var captiveRouter = require('./routes/captive');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var menuRouter = require('./routes/menu');
 
 var app = express();
 
@@ -17,7 +18,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(stylus.middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -25,14 +26,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/captive', captiveRouter);
 app.use('/users', usersRouter);
+app.use('/menu', menuRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ const meta = {
   title: 'El Culto al Perro Café',
   description: 'Cafetería Indie Experimental. Cafetería que entiende a la gente que trabaja. Hermosillo, Sonora.',
   url: 'https://cultoperrocafe.com',
-  image: 'https://cultoperrocafe.com/images/social.jpg'
+  image: 'https://cultoperrocafe.com/images/social.jpg',
 };
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', meta);
+  res.render('index', {...meta, path: req.originalUrl});
 });
 
 module.exports = router;
