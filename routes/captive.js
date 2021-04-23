6bec6f4454739a1ce23b653ceb61175e22dbd5fc
tonymtz <atony.mtz@gmail.com>
2021-04-22 20:36:15 -0700

    Captive page

--- routes/captive.js
+++ routes/captive.js
@@ -1,10 +1,29 @@
 var express = require('express');
+const MetaBuilder = require("../data/meta");
+const starred = require("../data/starred");
+
+const feed = [
+  'pins',
+  'hat',
+  'pin',
+  'cup'
+].map(item => `/images/pictures/swag-${ item }.jpg`);
+
 var router = express.Router();
-var meta = require("../data/meta");
+
+const metaBuilder = new MetaBuilder()
+  .setProp(MetaBuilder.TITLE, 'Bienvenidx')
+  .setProp(MetaBuilder.DESC, 'Portal de usuarios.')
+  .setProp(MetaBuilder.URL, '/home');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('captive', { ...meta, path: req.originalUrl });
+  res.render('captive', {
+    ...metaBuilder.build(),
+    path: req.originalUrl,
+    starred,
+    feed
+  });
 });
 
 module.exports = router;
