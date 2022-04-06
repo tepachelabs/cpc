f2ad734e6e170b0fe5cdf19465eeca7396daa424
tonymtz <atony.mtz@gmail.com>
2022-04-06 15:58:31 -0700

    Update package.json; links updated;

--- data/links.js
+++ data/links.js
@@ -2,6 +2,7 @@ const links = {
   'preguntas-frecuentes': 'https://cultoperrocafe.notion.site/Preguntas-frecuentes-f1b90c3be9ad46d58dc0716449f5d5b0',
   'manual-eventos': 'https://cultoperrocafe.notion.site/Manual-para-organizadores-de-eventos-39bfaae8281241aab5bf0898588e4e2d',
   'concurso-disfraces-2021': 'https://cultoperrocafe.notion.site/Reglamento-concurso-de-disfraces-de-mascotas-30-de-Octubre-2021-d0cdf7483acf41f994eb2e8790575779',
+  'contacto': 'https://forms.gle/fCmkQzX6TqUXXE9N6',
 };
 
 module.exports = links;
