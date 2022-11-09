558f5820b468326ebff5382c4bfc17d8e8d81f5e
tonymtz <atony.mtz@gmail.com>
2022-11-09 09:17:58 -0700

    Updates links

--- data/links.js
+++ data/links.js
@@ -1,7 +1,7 @@
 const links = {
-  'preguntas-frecuentes': 'https://cultoperrocafe.notion.site/Preguntas-frecuentes-f1b90c3be9ad46d58dc0716449f5d5b0',
-  'manual-eventos': 'https://cultoperrocafe.notion.site/Manual-para-organizadores-de-eventos-39bfaae8281241aab5bf0898588e4e2d',
-  'concurso-disfraces-2021': 'https://cultoperrocafe.notion.site/Reglamento-concurso-de-disfraces-de-mascotas-30-de-Octubre-2021-d0cdf7483acf41f994eb2e8790575779',
+  'preguntas-frecuentes': 'https://cultoperrocafe.notion.site/Preguntas-frecuentes-98f00f3e35e146c8abbf3383172009e9',
+  'manual-eventos': 'https://cultoperrocafe.notion.site/Manual-para-organizadores-de-eventos-38887170d9ff481c9d4e3f0b2c102930',
+  'concurso-disfraces-2021': 'https://cultoperrocafe.notion.site/Reglamento-concurso-de-disfraces-de-mascotas-30-de-Octubre-2021-521e4bd1d22d44898df2123c7d2cd40a',
   'contacto': 'https://forms.gle/fCmkQzX6TqUXXE9N6',
   'facturacion': 'https://forms.gle/mrzxTU8sRjcugrQi8',
   'tienda': 'https://tienda.cultoperrocafe.com',
