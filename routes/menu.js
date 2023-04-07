a225169f3dc80a156fff119a49266a4ac046b8de
tonymtz <atony.mtz@gmail.com>
2023-04-06 17:36:45 -0700

    Fixes 404

--- routes/menu.js
+++ routes/menu.js
@@ -1,45 +1,10 @@
 const express = require('express');
-const MetaBuilder = require("../data/meta");
-const menu = require("../data/menu");
 
 const router = express.Router();
 
-const metaBuilder = new MetaBuilder()
-  .setProp(MetaBuilder.TITLE, 'Nuestro Menú')
-  .setProp(MetaBuilder.DESC, 'Nuestros productos no se limitan a los aquí listados. Siempre estamos innovando en el laboratorio: nuestra cocina. Pregunta por nuestro menú experimental y las bebidas de temporada.')
-  .setProp(MetaBuilder.URL, '/menu')
-  .setProp(MetaBuilder.IMAGE, 'menu.png');
-
-const groupProductsByCategory = (products) => {
-  return products.reduce((acc, curr) => {
-    if (!acc[curr.category.title]) {
-      acc[curr.category.title] = [];
-    }
-    acc[curr.category.title].push(curr);
-    return acc;
-  }, {});
-};
-
-const formatPrice = (price) => (price / 100)
-const CATEGORY = {
-  COFFEE: 'Café',
-  NO_COFFEE: 'Sin café',
-  FOOD: 'Alimentos',
-  DESSERTS: 'Repostería',
-  METHODS: 'Especialidades',
-}
-
 /* GET home page. */
 router.get('/', async function (req, res) {
-  const { data: { products } } = (await menu.getProducts()).data;
-  res.render('menu', {
-    ...metaBuilder.build(),
-    version: req._VERSION,
-    path: req.originalUrl,
-    products: groupProductsByCategory(products),
-    formatPrice,
-    CATEGORY
-  });
+  res.redirect(302, 'https://instagram.com/cultoperrocafe')
 });
 
 module.exports = router;
